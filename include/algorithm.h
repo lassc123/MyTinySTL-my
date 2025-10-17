349f25efc91df6d6694426ec715eb1dc214dbc35
sh704 <3096079594@qq.com>
2025-10-17 13:35:05 +0800

    10-17

---
+++ include/algorithm.h
@@ -0,0 +1,14 @@
+#ifndef MYTINYSTL_ALGORITHM_H_
+#define MYTINYSTL_ALGORITHM_H_
+
+// 这个头文件包含了mystl的所有算法，包括基本算法，数值算法，heap算法，set算法和其他算法
+
+#include "algo.h"
+#include "algobase.h"
+// #include "set_algo.h"
+#include "heap_algo.h"
+// #include "numeric.h"
+
+namespace mystl {} // namespace mystl
+
+#endif // !MYTINYSTL_ALGORITHM_H_
